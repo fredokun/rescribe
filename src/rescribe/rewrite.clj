e233e896706b684818e1a1003ad21b7f329d00ba
Fredokun <fredokun@github.com>
2017-02-06 15:30:53 +0100

    new rewrite macro, better than the old defsystem

--- src/rescribe/rewrite.clj
+++ src/rescribe/rewrite.clj
@@ -1,5 +1,5 @@
-(ns rescribe.system
-  "This namespace defines the `defrewrite` macro that
+(ns rescribe.rewrite
+  "This namespace defines the `rewrite` macro that
  is the principal mean of encoding rewrite systems
   in *rescribe*."
   (:require [rescribe.term :refer [variable?
@@ -8,84 +8,121 @@
                                    assoc-term?
                                    subst
                                    rule-wff?]]
+            [rescribe.strategy :refer [or-else]]
             [rescribe.match :refer [match]]))
 
-(defn- rule-to-map
-  [rname lhs mid rhs]
-  (let [rule [(list 'quote lhs) (list 'quote rhs)]]
-    (if (not (apply rule-wff? rule))
-      (throw (ex-info "Malformed rule." {:rule-name rname :lhs lhs :rhs rhs})))
-    (if (not (= (name mid) "->"))
-      (throw (ex-info "Missing arrow -> in rule." {:rule-name rname})))
-    [(keyword (name rname)) rule]))
-
-
-(defn- check-rule
-  [rname lhs mid rhs]
-  (if (not (rule-wff? lhs rhs))
-    (throw (ex-info "Malformed rule." {:rule-name rname :lhs lhs :rhs rhs})))
-  (if (not (= (name mid) "->"))
-    (throw (ex-info "Missing arrow -> in rule." {:rule-name rname})))
-  true)
-
-(defn rewrite
+(defn apply-rewrite
   [term lhs rhs]
   (when-let [s (match lhs term)]
     (subst rhs s)))
 
-(defn- mk-rule-fun
-  [rname lhs _ rhs]
-  (let [tvar (gensym "term")]
-    `(defn ~rname [~tvar] (rewrite ~tvar (quote ~lhs) (quote ~rhs)))))
+(defn- check-rule
+  [rname lhs mid rhs]
+  (when (not (rule-wff? lhs rhs))
+    (throw (ex-info "Malformed rule." {:rule-name rname :lhs lhs :rhs rhs})))
+  (when (not (= (name mid) "->"))
+    (throw (ex-info "Missing arrow -> in rule." {:rule-name rname})))
+  true)
 
-(defn- map4
-  [f s] (if (seq s)
-          (let [args (take 4 s)]
-            (cons (apply f args)
-                  (map4 f (drop 4 s))))
-          '()))
+(defn parse-rule [rulemap name lhs mid rhs]
+  (check-rule name lhs mid rhs)
+  (when (get rulemap name)
+    (throw (ex-info "Duplicate rule name" {:rule-name name})))
+  (assoc rulemap name [lhs rhs]))
 
-(defn- mk-sys-strategy
-  [sys-name strat]
-  `(def ~(symbol (str sys-name "-strategy"))
-     ~strat))
+(defn gen-fresh [rulemap fresh]
+  (loop [candidate "_"]
+    (let [sym (symbol (str candidate fresh))]
+      (if (contains? rulemap sym)
+        (recur (str candidate "_"))
+        sym))))
 
-(defn- mk-sys-fun
-  [sys-name]
-  (let [term-param (gensym "term")
-        strat-param (gensym "strat")]
-    `(defn ~(symbol sys-name)
-       ~(str "Rewrite term with the optional strategy for system: " sys-name)
-       ([~term-param ~strat-param] (~strat-param ~term-param))
-       ([~term-param] (~(symbol (str sys-name "-strategy")) ~term-param)))))
+(defn parse-rules [args]
+  (loop [args args, fresh 0, rulemap {}, rulenames []]
+    (if (and (seq args) (not (keyword? (first args))))
+      (let [[rulename, lhs, mid, rhs, fresh', args']
+            (let [[arg1, arg2, arg3, arg4] args]
+              (if (symbol? arg1)
+                [arg1, arg2, arg3, arg4, fresh, (rest (rest (rest (rest args))))]
+                [(gen-fresh rulemap fresh), arg1, arg2, arg3, (inc fresh),
+                 (rest (rest (rest args)))]))]
+        (recur args' fresh' (parse-rule rulemap rulename lhs mid rhs)
+               (conj rulenames rulename)))
+      [rulemap, rulenames, args])))
 
+;; (parse-rules '[lplus-zero (0 + ?X) -> ?X
+;;                rplus-zero (?X + 0) -> ?X
+;;                ltimes-one (?X * 1) -> ?X
+;;                rtimes-one (1 * ?X) -> ?X])
 
-(defn parse-rule [rulemap fresh name lhs rhs]
-  )
+;; (parse-rules '[lplus-zero (0 + ?X) -> ?X
+;;                rplus-zero (?X + 0) -> ?X
+;;                ltimes-one (?X * 1) -> ?X
+;;                rtimes-one (1 * ?X) -> ?X
+;;                :strategy success])
 
-(defn parse-rules [rules]
-  (loop [rules rules, rulemap {}]
-    (if (seq rules)
-      (let [[arg1 arg2 arg3 arg4] (first rules)]
-        ()
-        (recur (rest rules) (parse-rule rulemap (first rules))))
-      rulemap)))
-
-(defn parse-defrewrite
-  [rules opts]
-  (let [rulemap (parse-rules rules)
-        [with-kw strat] opts]
-    (when (and with-kw (not= with-kw :with))
-      (throw (ex-info "Malformed `defrewrite`, `:with` keyword expected"
-                      {:opts opts})))
+(defn parse-rewrite
+  [args]
+  (let [[rulemap, rulenames, args'] (parse-rules args)
+        [strat-kw strat] args']
+    (when (and strat-kw (not (#{:strategy :strat} strat-kw)))
+      (throw (ex-info "Malformed `defrewrite`, `:strategy` keyword expected"
+                      {:args args'})))
     {:rules rulemap
-     :strat strat}))
+     :rule-names rulenames
+     :strategy strat}))
 
-(defmacro defrewrite
+(defn- mk-rule-letfun
+  [rname lhs rhs]
+  (let [args (gensym "args")]
+    `(~rname [& ~args]
+     (case (count ~args)
+       0 [~rname (quote ~lhs) (quote ~rhs)]
+       1 (apply-rewrite (first ~args) (quote ~lhs) (quote ~rhs))
+       (throw (ex-info "Wrong number of arguments (expecting 0 or 1)" {:args ~args}))))))
+
+;; (mk-rule-letfun 'plus-zero '(?X + 0) '?X)
+
+(defn mk-default-strategy [rule-names]
+  `(or-else ~@rule-names))
+
+;; (mk-default-strategy '[lplus-zero rplus-zero ltimes-one rtimes-one])
+
+(defmacro rewrite
   "Definition of a rewrite system."
-  [name [& rules] & opts]
-  `(do
-     ~@(map4 mk-rule-fun rules)
-     ~(mk-sys-strategy sys-name strat)
-     ~(mk-sys-fun sys-name)))
+  [& args]
+  (let [{:keys [rules rule-names strategy]} (parse-rewrite args)
+        strat-fn (or strategy (mk-default-strategy rule-names))
+        rule-funs (map (fn [[rname [lhs rhs]]]
+                         (mk-rule-letfun rname lhs rhs)) rules)
+        params (gensym "params")
+        strat-fn-name (gensym "strat-fn")]
+    `(letfn [~@rule-funs]
+       (let [~strat-fn-name ~strat-fn]
+         (fn [& ~params]
+           (case (count ~params)
+             0 ~strat-fn-name
+             1 (~strat-fn-name (first ~params))
+             (throw (ex-info "Wrong number of arguments (expecting 0 or 1)" {:args ~params}))))))))
+
+;; (macroexpand-1 '(rewrite
+;;                  lplus-zero (0 + ?X) -> ?X
+;;                  rplus-zero (?X + 0) -> ?X
+;;                  ltimes-one (?X * 1) -> ?X
+;;                  rtimes-one (1 * ?X) -> ?X))
+
+;; ((rewrite
+;;   lplus-zero (0 + ?X) -> ?X
+;;   rplus-zero (?X + 0) -> ?X
+;;   ltimes-one (?X * 1) -> ?X
+;;   rtimes-one (1 * ?X) -> ?X)
+;;  '(0 + 4))
+
+;; ((rewrite
+;;   lplus-zero (0 + ?X) -> ?X
+;;   rplus-zero (?X + 0) -> ?X
+;;   ltimes-one (?X * 1) -> ?X
+;;   rtimes-one (1 * ?X) -> ?X)
+;;  42)
+
 
